09de9b8cb9b63502ffab9db9fc0409cc45769c39
wermos <63574588+wermos@users.noreply.github.com>
2023-12-11 12:09:36 +0530

    Stripped trailing whitespace.

--- 2023/day1-part1.py
+++ 2023/day1-part1.py
@@ -7,12 +7,12 @@ regex = re.compile(r"\d")
 with open("day1.txt", "r") as f:
     for line in f.readlines():
         first = re.search(regex, line)[0]
-        
+
         reversed_line = ''.join(list(reversed(line)))
         last = re.search(regex, reversed_line)[0]
-        
+
         number = int(''.join([first, last]))
-        
+
         total += number
 
 print(total)
