874866a578a31422a96faf7d50c85723307e16b4
wermos <63574588+wermos@users.noreply.github.com>
2023-12-23 11:38:35 +0530

    Trimmed trailing whitespace and renamed the file to conform to the naming scheme.

--- 2023/day15_part1.py
+++ 2023/day15_part1.py
@@ -1,11 +1,11 @@
 def hash(string: str) -> int:
     current_value = 0
-    
+
     for char in string:
         current_value += ord(char)
         current_value *= 17
         current_value %= 256
-    
+
     return current_value
 
 def read_input(path: str) -> list[str]:
@@ -13,8 +13,8 @@ def read_input(path: str) -> list[str]:
         # There is no newline in the inputs for this problem: Neither
         # in the test input nor the actual input.
         return f.readline().split(",")
-    
+
 if __name__ == "__main__":
     string_list = read_input("inputs/day15.txt")
-    
+
     print(sum(map(hash, string_list)))
