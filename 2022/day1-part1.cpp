e8a0f92ece4ae08ff8152d0acb36742798f58091
wermos <63574588+wermos@users.noreply.github.com>
2024-01-28 06:41:02 +0530

    Solved Day 1's Part 1 for 2023.

---
+++ 2022/day1-part1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream input{"inputs/day1.txt"};
+
+    // we parse the input and to the computations required at the same time
+    int running_sum = 0;
+    int highest = 0;
+
+    std::string line;
+
+    while (std::getline(input, line)) {
+        if (line == "") {
+            if (running_sum > highest) {
+                highest = running_sum;
+            }
+            // we reset the running sum to 0 for the next elf
+            running_sum = 0;
+        } else {
+            int num = std::stoi(line);
+            running_sum += num;
+        }
+    }
+
+    std::cout << highest << std::endl;
+}
