7bf61e2d90feec3a0b7e092f6854940c15c40c3a
wermos <63574588+wermos@users.noreply.github.com>
2024-04-07 14:21:45 +0530

    Formatted the files using `clang-format`.

--- 2022/day5_part1.cpp
+++ 2022/day5_part1.cpp
@@ -1,8 +1,8 @@
+#include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <cstdint>
-#include <string>
 #include <stack>
+#include <string>
 
 template <std::uint8_t N>
 struct CrateStack {
@@ -14,18 +14,16 @@ struct CrateStack {
 template <std::uint8_t N>
 void initialize(const std::ifstream& file, const CrateStack<N>& stack, const bool test = false) {
     if constexpr (test) {
-        // then the first few 
+        // then the first few
     } else {
-        
     }
 }
 
 int main() {
     std::ifstream input{"inputs/day5-test.txt"};
-    
+
     std::string line;
 
     while (std::getline(input, line)) {
-    
     }
 }
--- 2022/day7_part1.cpp
+++ 2022/day7_part1.cpp
@@ -1,85 +1,74 @@
-#include <fstream>
 #include <cstdint>
+#include <fstream>
 #include <iostream>
-#include <string>
 #include <memory>
+#include <string>
 #include <unordered_map>
 
 class fNode;
 
 class File {
-    public:
-        File() : parent{nullptr}, name{}, size{0} {}
+   public:
+    File() : parent{nullptr}, name{}, size{0} {}
 
-        File(std::string name, std::shared_ptr<fNode> parent) : parent{parent}, name{name}, size{0} {}
+    File(std::string name, std::shared_ptr<fNode> parent) : parent{parent}, name{name}, size{0} {}
 
-        void updateSize(std::uint64_t new_size) {
-            size = new_size;
-        }
+    void updateSize(std::uint64_t new_size) { size = new_size; }
 
-    private:
-        std::shared_ptr<fNode> parent;
+   private:
+    std::shared_ptr<fNode> parent;
 
-        std::string name;
-        std::uint64_t size;
+    std::string name;
+    std::uint64_t size;
 };
 
 class fNode {
-    public:
-        fNode() : parent{nullptr}, subdirectories{}, files{}, name{}, size{0} {}
+   public:
+    fNode() : parent{nullptr}, subdirectories{}, files{}, name{}, size{0} {}
 
-        fNode(std::string name, std::shared_ptr<fNode> parent) : parent{parent}, subdirectories{}, files{}, name{name}, size{0} {}
+    fNode(std::string name, std::shared_ptr<fNode> parent)
+        : parent{parent}, subdirectories{}, files{}, name{name}, size{0} {}
 
-        void addFile(std::string name, bool isFile) {
-            files[name] = std::make_shared<File>(name, this);
+    void addFile(std::string name, bool isFile) { files[name] = std::make_shared<File>(name, this); }
+
+    void addSubdirectory(std::string name, bool isFile) { subdirectories[name] = std::make_shared<fNode>(name, this); }
+
+    void updateSize(std::string name, bool isFile, std::uint64_t size) {
+        if (isFile) {
+            files[name]->updateSize(size);
+        } else {
+            subdirectories[name]->updateSize(size);
         }
+    }
 
-        void addSubdirectory(std::string name, bool isFile) {
-            subdirectories[name] = std::make_shared<fNode>(name, this);
-        }
-
-        void updateSize(std::string name, bool isFile, std::uint64_t size) {
-            if (isFile) {
-                files[name]->updateSize(size);
-            } else {
-                subdirectories[name]->updateSize(size);
-            }
-        }
-
-        void updateSize(std::uint64_t new_size) {
-            size = new_size;
-        }
+    void updateSize(std::uint64_t new_size) { size = new_size; }
 
     // private:
-        std::shared_ptr<fNode> parent;
-        std::unordered_map<std::string, std::shared_ptr<fNode>> subdirectories;
-        std::unordered_map<std::string, std::shared_ptr<File>> files;
+    std::shared_ptr<fNode> parent;
+    std::unordered_map<std::string, std::shared_ptr<fNode>> subdirectories;
+    std::unordered_map<std::string, std::shared_ptr<File>> files;
 
-        std::string name;
-        std::uint64_t size;
+    std::string name;
+    std::uint64_t size;
 };
 
 class FileSystem {
-    public:
-        FileSystem() = default;
+   public:
+    FileSystem() = default;
 
-        void initialize() {
-            root = std::make_shared<fNode>();
+    void initialize() {
+        root = std::make_shared<fNode>();
 
-            current = root;
-        }
+        current = root;
+    }
 
-        void cd(std::string name) {
-            current = root->subdirectories[name];
-        }
+    void cd(std::string name) { current = root->subdirectories[name]; }
 
-        void update() {
-            
-        }
+    void update() {}
 
-    private:
-        std::shared_ptr<fNode> root;
-        std::weak_ptr<fNode> current;
+   private:
+    std::shared_ptr<fNode> root;
+    std::weak_ptr<fNode> current;
 };
 
 int main() {
@@ -99,14 +88,12 @@ int main() {
                 if (dir_name == "/") {
                     fs.initialize();
                 } else {
-
                 }
 
             } else if (line.substr(2, 4) == "ls") {
                 // ls_output = true;
             }
         } else {
-
         }
     }
 }
