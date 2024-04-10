cdb4638070121a99c5b3abd621a792a401b8c049
wermos <63574588+wermos@users.noreply.github.com>
2024-04-11 04:53:48 +0530

    Small modification to the hook.

--- hooks/count.py
+++ hooks/count.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import re
 import subprocess
@@ -34,14 +35,11 @@ def calculate_progress(counts):
 
 def calculate_overall_progress(year_counts, total_years):
     expected_files = 25 * 2 * total_years
-    # number of problems per year, times the number of parts, time the number of years
-
-    # print([data for data in year_counts.values()])
     actual_files = sum([data for data in year_counts.values()])
     overall_progress_percentage = min(math.floor((actual_files / expected_files) * 100), 100)
     return overall_progress_percentage
 
-def count_files_in_years(root_directory, valid_extensions):
+def count_files_in_years(root_directory, valid_extensions, dry_run=False):
     year_pattern = re.compile(r'^\d{4}$')
     year_progress = {}
 
@@ -52,7 +50,10 @@ def count_files_in_years(root_directory, valid_extensions):
             counts = count_files(year_path, valid_extensions)
             progress = calculate_progress(counts)
             year_progress[subdir] = progress
-            # print(f"Year {subdir}: Part 1 - {counts['1']}, Part 2 - {counts['2']}, Progress - {progress}%")
+        
+            if dry_run:
+                # print(f"Year: {subdir}, Progress: {year_progress[subdir]}")
+                print(f"Year {subdir}: Part 1 - {counts['1']}, Part 2 - {counts['2']}, Progress - {progress}%")
 
     return year_progress
 
@@ -60,12 +61,10 @@ def update_readme_with_progress(readme_path, year_progress, overall_progress):
     with open(readme_path, 'r') as file:
         readme_content = file.read()
 
-    # Update overall progress
     overall_pattern = re.compile(r'!\[Progress Bar to show how much progress has been made\]\(https://progress-bar.dev/\d+/\?title=Progress\)')
     overall_content = f"![Progress Bar to show how much progress has been made](https://progress-bar.dev/{overall_progress}/?title=Progress)"
     readme_content = overall_pattern.sub(overall_content, readme_content)
 
-    # Update progress for each year
     for year, progress in year_progress.items():
         year_pattern = re.compile(fr'!\[Progress Bar to show how much progress has been made in the {year} problems\]\(https://progress-bar.dev/\d+/\?title={year}\)')
         year_content = f"![Progress Bar to show how much progress has been made in the {year} problems](https://progress-bar.dev/{progress}/?title={year})"
@@ -74,15 +73,11 @@ def update_readme_with_progress(readme_path, year_progress, overall_progress):
     with open(readme_path, 'w') as file:
         file.write(readme_content)
 
-    with open(readme_path, 'w') as file:
-        file.write(readme_content)
-
 def commit_readme_changes(commit_message, readme_path):
     os.system(f"git add {readme_path}")
     os.system(f"git commit -m \"{commit_message}\"")
-    # print(f"git commit -m \"{commit_message}\"")
 
-def run_clang_format(directory, file_extensions, target_directories):
+def run_clang_format(directory, file_extensions, target_directories, dry_run=False):
     formatted_files = []
     for root, _, filenames in os.walk(directory):
         if root.endswith(tuple(target_directories)):
@@ -90,38 +85,36 @@ def run_clang_format(directory, file_extensions, target_directories):
                 if filename.endswith(tuple(file_extensions)):
                     file_path = os.path.join(root, filename)
                     if is_file_tracked(file_path):
-                        os.system(f"clang-format --style=file -i {file_path}")
-                        # print(f"clang-format --style=file -i {file_path}")
+                        if not dry_run:
+                            os.system(f"clang-format --style=file -i {file_path}")
                         formatted_files.append(file_path)
 
     return formatted_files
 
-def commit_formatted_files(formatted_files, commit_message):
-    if formatted_files:
+def commit_formatted_files(formatted_files, commit_message, dry_run=False):
+    if formatted_files and not dry_run:
         formatted_files_str = ' '.join(formatted_files)
         os.system(f"git add {formatted_files_str}")
         os.system(f"git commit -m \"{commit_message}\"")
-        # print(f"git add {formatted_files_str}")
-        # print(f"git commit -m \"{commit_message}\"")
 
 def main():
+    parser = argparse.ArgumentParser(description='Script for updating progress and formatting code.')
+    parser.add_argument('--dry_run', '-d', action='store_true', help='Run in dry-run mode (no actual changes will be made)')
+    args = parser.parse_args()
+
     root_directory = "."
     valid_extensions = ['.cpp', '.java', '.py', '.rs']
 
-    year_progress = count_files_in_years(root_directory, valid_extensions)
+    year_progress = count_files_in_years(root_directory, valid_extensions, dry_run=args.dry_run)
     total_years = len(year_progress)
 
-    # Run clang-format on .cpp and .hpp files
     target_directories = ['2022', '2023/src']
-    formatted_files = run_clang_format(root_directory, ['.cpp', '.hpp'], target_directories)
-    commit_formatted_files(formatted_files, r"Formatted the files using \`clang-format\`.")
+    formatted_files = run_clang_format(root_directory, ['.cpp', '.hpp'], target_directories, dry_run=args.dry_run)
+    commit_formatted_files(formatted_files, r"Formatted the files using \`clang-format\`.", dry_run=args.dry_run)
 
-    # Calculate and print the overall progress
     overall_progress = calculate_overall_progress(year_progress, total_years)
-    # print(f"Overall Progress: {overall_progress}%")
 
-    # Update README with progress and commit changes
-    readme_path = "./README.md"  # Update with the actual path to your README
+    readme_path = "./README.md"
     update_readme_with_progress(readme_path, year_progress, overall_progress)
     commit_message = "Updated README to reflect current progress using pre-push git hook."
     commit_readme_changes(commit_message, readme_path)
