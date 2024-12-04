8e56b1603ccaf2c06d2325639e33be41bce58842
wermos <63574588+wermos@users.noreply.github.com>
2024-12-04 14:20:18 +0530

    Solved Part1 of Days 2 and 3 for 2024.

---
+++ 2024/src/day2_part1.rs
@@ -0,0 +1,75 @@
+use std::fs;
+
+fn convert(line: &str) -> Vec<i32> {
+    line.split_whitespace().map(|e| e.parse::<i32>().unwrap()).collect()
+}
+
+fn is_safe(list: &[i32]) -> bool {
+    // we can safely assume that `list` has at least 2 elements.
+    let increasing;
+
+    if list[1] > list[0] {
+        increasing = true;
+    } else {
+        increasing = false;
+    };
+
+    if increasing {
+        for i in 1..list.len() {
+            let diff = list[i] - list[i - 1];
+    
+            if diff <= 0 {
+                // in this case, either the list had the same number twice in a
+                // row, or started decreasing all of a sudden
+                return false;
+            }
+    
+            // we know that `diff` must be at least 1 by virtue of there being
+            // no integer between 0 and 1.
+            if diff > 3 {
+                return false;
+            }
+        }
+    } else {
+        // then the list is decreasing
+        for i in 1..list.len() {
+            let diff = list[i - 1] - list[i];
+    
+            if diff <= 0 {
+                // in this case, either the list had the same number twice in a
+                // row, or started increasing all of a sudden
+                return false;
+            }
+    
+            // we know that `diff` must be at least 1 by virtue of there being
+            // no integer between 0 and 1.
+            if diff > 3 {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn main() {
+    let filename;
+
+    if cfg!(debug_assertions) {
+        filename = "inputs/day2-test.txt";
+    } else {
+        filename = "inputs/day2.txt";
+    }
+
+    let input = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let mut num_safe_lines = 0;
+
+    for line in input.lines() {
+        if is_safe(&convert(line)) {
+            num_safe_lines += 1;
+            // println!("{:?}", line);
+        }
+    }
+
+    println!("{num_safe_lines}");
+}
---
+++ 2024/src/day3_part1.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use regex::Regex;
+
+fn main() {
+    let filename;
+
+    if cfg!(debug_assertions) {
+        filename = "inputs/day3-test.txt";
+    } else {
+        filename = "inputs/day3.txt";
+    }
+
+    let input = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let re = Regex::new(r"mul\((?P<arg1>\d+),(?P<arg2>\d+)\)").unwrap();
+
+    let mut sum = 0;
+
+    for m in re.captures_iter(input.as_str()) {
+        // println!("{:?}", m);
+
+        let arg1 = m["arg1"].parse::<i64>().unwrap();
+        let arg2 = m["arg2"].parse::<i64>().unwrap();
+
+        // println!("{:?} and {:?}", arg1, arg2);
+
+        sum += arg1 * arg2;
+    }
+
+    println!("{sum}");
+}
