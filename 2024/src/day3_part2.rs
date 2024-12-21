438da565902054ea5afa071e18de851f839b5151
wermos <63574588+wermos@users.noreply.github.com>
2024-12-21 07:52:15 +0530

    Rewrote 2024's Day 3 Part 2 implementation.

--- 2024/src/day3_part2.rs
+++ 2024/src/day3_part2.rs
@@ -15,28 +15,23 @@ fn main() {
     let input = raw_input.as_str();
 
     // `re` is used to find the initial matches
-    let re = Regex::new(r"don't\(\)|do\(\)|mul\(\d+,\d+\)").unwrap();
-    // `mul_re` is used in the main loop. We define it out here to prevent recompilation
-    // of the regex string at every iteration.
-    let mul_re = Regex::new(r"mul\((?P<arg1>\d+),(?P<arg2>\d+)\)").unwrap();
-
-    let matches = re.find_iter(input);
+    let re = Regex::new(r"don't\(\)|do\(\)|mul\((?P<arg1>\d+),(?P<arg2>\d+)\)").unwrap();
+    
+    let matches = re.captures_iter(input);
     let mut active = true;
 
     let mut sum = 0;
 
-    for m in matches {
-        match m.as_str() {
+    for cap in matches {
+        match cap.get(0).unwrap().as_str() {
             "do()" => active = true,
             "don't()" => active = false,
             // that means we matched on a `mul`
             _ => if active {
                 // run the regex match function again, but this time we capture the extra,
                 // necessary info, in capture groups
-
-                let m = mul_re.captures_at(input, m.start()).unwrap();
-                let arg1 = m["arg1"].parse::<i64>().unwrap();
-                let arg2 = m["arg2"].parse::<i64>().unwrap();
+                let arg1 = cap["arg1"].parse::<i64>().unwrap();
+                let arg2 = cap["arg2"].parse::<i64>().unwrap();
 
                 sum += arg1 * arg2;
             },
